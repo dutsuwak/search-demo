ba8b4af5722e5994f23a22e1e571dcde72f3b667
Fabian Solano <fabian.solano@ridgerun.com>
2021-12-19 12:20:32 -0600

    Add framework interface class to support multiple search types

--- src/main/java/com/search/demo/controller/SearchController.java
+++ src/main/java/com/search/demo/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.search.demo.controller;
 
 import com.search.demo.service.SearchService;
+import com.search.demo.controller.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
---
+++ src/main/java/com/search/demo/frameworks/iSearchFramework.java
@@ -0,0 +1,44 @@
+package com.search.demo.frameworks;
+
+import java.util.List;
+
+import com.search.demo.controller.Movie;
+
+/**
+ * Interface that allows the support of multiple
+ * search engines and frameworks.
+ *
+ * This is the common interface for frameworks family.
+ */
+public interface iSearchFramework {
+
+    /**
+     * Initializes the framework based on the required search configuration
+     * 
+     * @param searchArg indicates initialization argument for each framework
+     */
+    void setup(String searchArg);
+
+    /**
+     * Indexes information by the name of the movie
+     * 
+     * @param title search argument
+     */
+    Movie searchByTitle(String title);
+
+    /**
+     * Indexes information by the rate of the movie based on
+     * the low and high rate filters as [low, high[
+     * 
+     * @param low lower range including this value
+     * @param high higher range excluding this value
+     */
+    List<Movie> searchByRate(int low, int high);
+
+    /**
+     * Provides the information by the matching genre of the movie
+     * 
+     * @param genre string to search for a maching value
+     */
+    List<Movie> searchByGenre(String genre);
+}
